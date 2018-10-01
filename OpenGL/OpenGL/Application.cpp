87e29cccd8f359e193d44c874e96e630c9242fa2
Nettozx <nettomajid@gmail.com>
2018-09-30 17:53:53 -0700

    attribpointers triangle visible again

--- OpenGL/OpenGL/Application.cpp
+++ OpenGL/OpenGL/Application.cpp
@@ -30,7 +30,7 @@ int main(void)
 
 	//triangle positions
 	float positions[6] = {
-	   -0.5f, -0.5f,
+	   -0.5f, -0.5f, //x,y of vertex
 		0.0f,  0.5f,
 		0.5f, -0.5f
 	};
@@ -40,6 +40,16 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, buffer); //select buffer
 	//put data in buffer
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	
+	//enable vertexattrib
+	glEnableVertexAttribArray(0);
+	//index 0 because its first attirbute
+	//2 coordinate position so size is 2
+	//type of positions is floats
+	//no normalization yet
+	//amount of offset to get to the next vertex = 2 floats the x and y
+	//no offset
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
