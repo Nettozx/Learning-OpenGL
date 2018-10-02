b9ee3e39a155b6855f28d8d167a95c5000946c5f
Nettozx <nettomajid@gmail.com>
2018-10-02 11:59:20 -0700

    move macros into renderer

--- OpenGL/OpenGL/Application.cpp
+++ OpenGL/OpenGL/Application.cpp
@@ -5,26 +5,7 @@
 #include <string>
 #include <sstream>
 
-#define ASSERT(x) if (!(x)) __debugbreak();
-#define GLCall(x) GLClearError();\
-	x;\
-	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
-
-static void GLClearError()
-{
-	while (glGetError() != GL_NO_ERROR);
-};
-
-static bool GLLogCall(const char* function, const char* file, int line)
-{
-	while (GLenum error = glGetError())
-	{
-		std::cout << "[OpenGL Error] (" << error << "): " << function <<
-			" " << file << ":" << line << "\n";
-		return false;
-	}
-	return true;
-}
+#include "Renderer.h"
 
 //so we can return multiple things
 struct ShaderProgramSource
