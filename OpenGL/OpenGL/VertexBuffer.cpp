489abb8b22f7833084fcd4f62701c6eb2860997d
Nettozx <nettomajid@gmail.com>
2018-10-02 12:11:38 -0700

    added vertex buffer class

---
+++ OpenGL/OpenGL/VertexBuffer.cpp
@@ -0,0 +1,25 @@
+#include "VertexBuffer.h"
+#include "Renderer.h"
+
+VertexBuffer::VertexBuffer(const void * data, unsigned int size)
+{
+	GLCall(glGenBuffers(1, &m_RendererID)); //generate buffer with id and address
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID)); //select buffer
+	//put data in buffer
+	GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
+}
+
+VertexBuffer::~VertexBuffer()
+{
+	GLCall(glDeleteBuffers(1, &m_RendererID));
+}
+
+void VertexBuffer::Bind()
+{
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, m_RendererID));
+}
+
+void VertexBuffer::Unbind()
+{
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+}
